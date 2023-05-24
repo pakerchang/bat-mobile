0c7b015ed860a2e4a7df52a4fb51f9b4e3920c11
pzdesu <pakerchang@gmail.com>
2023-05-24 16:37:02 +0800

    feat: #4 Add cities

--- ubike/app/page.tsx
+++ ubike/app/page.tsx
@@ -6,13 +6,27 @@ import StationInfo from "@/components/station-info";
 import Data from "./assets/station-info/ubike.json";
 import useTaipeiData from "./hooks/useTaipeiData";
 
+const cities: string[] = [
+  "台北市",
+  "新北市",
+  "桃園市",
+  "新竹市",
+  "新竹縣",
+  "苗栗縣",
+  "嘉義市",
+  "台南市",
+  "高雄市",
+  "屏東縣",
+  "金門縣",
+];
+
 export default function Home() {
   const taipeiData = useTaipeiData(Data);
 
   return (
     <Box display="flex" flexDirection="column" width="100vw" height="100vh">
       <Header />
-      <StationInfo data={taipeiData} />
+      <StationInfo cities={cities} data={taipeiData} />
     </Box>
   );
 }
