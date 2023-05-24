949ed0552929285b5ca65e593c0c54777e9ecc3e
pzdesu <pakerchang@gmail.com>
2023-05-24 16:38:40 +0800

    feat(custom): feat: Station info props.

--- ubike/components/station-info/SearchBar.tsx
+++ ubike/components/station-info/SearchBar.tsx
@@ -1,16 +1,28 @@
 "use client";
 import React, { useState } from "react";
-import { Box, Select, FormControl, InputLabel, MenuItem, Autocomplete, TextField } from "@mui/material";
-import { NewDataType } from "../shared/types/index";
+import {
+  Box,
+  Select,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Autocomplete,
+  TextField,
+  SelectChangeEvent,
+} from "@mui/material";
+import { NewDataType } from "../types";
 
 interface SearchBarProps<T> {
-  city: string[];
+  cities: string[];
   areaData: NewDataType;
   handleSearch: () => T[];
 }
 
-function SearchBar<T>({ city, areaData }: SearchBarProps) {
+function SearchBar<T>({ cities, areaData }: SearchBarProps<T>) {
   const [selectData, setSelectData] = useState<NewDataType[]>(areaData);
+  const [selectCity, setCity] = useState("");
+
+  const onSelectCity = (e: SelectChangeEvent) => setCity(e.target.value);
 
   const comboSearch = () => {
     // handling select to update Autocomplete
@@ -19,7 +31,13 @@ function SearchBar<T>({ city, areaData }: SearchBarProps) {
     <Box display="flex" mb="32px">
       <FormControl sx={{ width: "175px", height: "40px" }}>
         <InputLabel id="search-city">選擇縣市</InputLabel>
-        <Select labelId="search-city"></Select>
+        <Select labelId="search-city" label="選擇縣市" value={selectCity} onChange={onSelectCity}>
+          {cities.map((item) => (
+            <MenuItem key={item} value={item}>
+              {item}
+            </MenuItem>
+          ))}
+        </Select>
       </FormControl>
       <Autocomplete
         id="search-station"
@@ -31,7 +49,7 @@ function SearchBar<T>({ city, areaData }: SearchBarProps) {
           </li>
         )}
         renderInput={(params) => <TextField {...params} label="搜尋站點" />}
-        // disabled={!selectionData?.area && true}
+        disabled={!selectData && true}
         sx={{ width: "277px", height: "40px", marginLeft: "16px" }}
       />
     </Box>
