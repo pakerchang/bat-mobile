8953c0247f2a349f8d09705add054f2aeb835413
pzdesu <pakerchang@gmail.com>
2023-05-25 22:17:09 +0800

    style: adjust link style

--- ubike/app/styles/muiTheme.ts
+++ ubike/app/styles/muiTheme.ts
@@ -5,6 +5,15 @@ const theme = createTheme({
     primary: { main: "#B5CC22" },
     secondary: { main: "#677510" },
   },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        a: {
+          textDecoration: "none"
+        }
+      }
+    }
+  }
 });
 
 export default theme;
